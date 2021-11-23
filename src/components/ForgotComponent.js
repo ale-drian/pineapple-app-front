e1e8178a2e623ddb301f785c3e2249c79bdfade3
Mariam Apaza Santillana <mariam.apaza@tecsup.edu.pe>
2021-11-23 02:56:45 -0500

    Forgot Component Edited

--- src/components/ForgotComponent.js
+++ src/components/ForgotComponent.js
@@ -9,6 +9,8 @@ import {
     Text,
     useColorModeValue,
   } from '@chakra-ui/react';
+  import mySvg from '../images/background.png';
+    import svg2 from '../bg-login4.png';
 
   function Forgot() {
     return (
@@ -16,7 +18,9 @@ import {
       minH={'100vh'}
       align={'center'}
       justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
+      bg={useColorModeValue('gray.50', 'gray.800')}
+      position={'relative'} backgroundImage={`url(${mySvg})`} backgroundPosition="center"
+      backgroundSize="cover" backgroundRepeat="no-repeat" width="100vw">
       <Stack
         spacing={4}
         w={'full'}
@@ -27,26 +31,26 @@ import {
         p={6}
         my={12}>
         <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
-          Forgot your password?
+          ¿Olvidaste tu contraseña?
         </Heading>
         <Text
           fontSize={{ base: 'sm', sm: 'md' }}
           color={useColorModeValue('gray.800', 'gray.400')}>
-          You&apos;ll get an email with a reset link
+            Ingresa el correo asociado a tu cuenta
         </Text>
         <FormControl id="email">
           <Input
-            placeholder="your-email@example.com"
+            placeholder="email@example.com"
             _placeholder={{ color: 'gray.500' }}
             type="email"
           />
         </FormControl>
         <Stack spacing={6}>
           <Button
-            bg={'blue.400'}
+            bg={"#FF5757"}
             color={'white'}
             _hover={{
-              bg: 'blue.500',
+              bg: 'black',
             }}>
             Request Reset
           </Button>
