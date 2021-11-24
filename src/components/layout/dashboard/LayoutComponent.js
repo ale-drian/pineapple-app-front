159ea1cd47d00500fbd98c5a9aa94d2f44ce0d8f
Alejandra Adrian Tejada <thalia.adrian@tecsup.edu.pe>
2021-11-24 15:51:20 -0500

    drawer on resposive

--- src/components/layout/dashboard/LayoutComponent.js
+++ src/components/layout/dashboard/LayoutComponent.js
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router';
 import { useAuthContext } from '../../../App';
 
 import {
-    Box, useColorModeValue, useDisclosure
+    Box, useColorModeValue, useDisclosure, Drawer, DrawerContent
 } from '@chakra-ui/react';
 import {
     FaHome, FaUserFriends, FaBoxOpen
@@ -20,7 +20,8 @@ const Layout = () => {
         ... user.role.id == 1 ? [{ name: 'Usuarios', icon: FaUserFriends, ref: '/dashboard/users' }] : [],
         { name: 'Productos', icon: FaBoxOpen, ref: '/dashboard/products' },
     ];
-    const { onOpen, onClose } = useDisclosure();
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
     
 
     return (
@@ -31,6 +32,19 @@ const Layout = () => {
                 display={{ base: 'none', md: 'block' }}
                 LinkItems={LinkItems}
             />
+            <Drawer
+                autoFocus={false}
+                isOpen={isOpen}
+                placement="left"
+                onClose={onClose}
+                returnFocusOnClose={false}
+                onOverlayClick={onClose}
+                size="full">
+                <DrawerContent>
+                    <Sidebar onClose={onClose} 
+                        LinkItems={LinkItems}/>
+                </DrawerContent>
+            </Drawer>
             {/* Header */}
             <Header onOpen={onOpen} />
             <Box ml={{ base: 0, md: 60 }} p="4">
